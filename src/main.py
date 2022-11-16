02373f6a1640dae7d478bb333871675a4a52288b
Andrew Johnston <asjohnston@alaska.edu>
2022-11-16 11:43:02 -0900

    cloudformation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-import requests
+import os
 
 import boto3
+import requests
 from remotezip import RemoteZip
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
@@ -10,12 +11,12 @@ response.raise_for_status()
 credentials = response.json()
 
 auth = AWSRequestsAuth(
-         aws_service='s3',
-         aws_host='s3-us-west-2.amazonaws.com',
-         aws_region='us-west-2',
-         aws_access_key=credentials['AccessKeyId'],
-         aws_secret_access_key=credentials['SecretAccessKey'],
-         aws_token=credentials['SessionToken'],
+    aws_service='s3',
+    aws_host='s3-us-west-2.amazonaws.com',
+    aws_region='us-west-2',
+    aws_access_key=credentials['AccessKeyId'],
+    aws_secret_access_key=credentials['SecretAccessKey'],
+    aws_token=credentials['SessionToken'],
 )
 
 s3 = boto3.client('s3')
@@ -24,6 +25,7 @@ target_bucket = 'asj-dev'
 
 def process_granules(granules):
     for granule in granules:
+        print(granule)
         url = f'https://s3-us-west-2.amazonaws.com/asf-ngap2w-p-s1-slc-7b420b89/{granule}.zip'
         with RemoteZip(url, auth=auth) as z:
             filenames = z.namelist()
@@ -32,6 +34,7 @@ def process_granules(granules):
                     print(filename)
                     z.extract(filename)
                     s3.upload_file(filename, target_bucket, filename)
+                    os.remove(filename)
 
 
 def lambda_handler(event, context):
